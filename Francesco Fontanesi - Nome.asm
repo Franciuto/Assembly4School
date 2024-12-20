40401f22003ffe06f4c464b5a6541b2d0933928c
Francesco Fontanesi <francesco.traversetolo@gmail.com>
2024-12-20 15:02:25 -0800

    Add files via upload

---
+++ Francesco Fontanesi - Nome.asm
@@ -0,0 +1,66 @@
+MOV AH, 2
+ 
+
+MOV DL, 'F'
+int 21h
+MOV DL, 'r'
+int 21h
+mov DL , 'a'
+int 21h
+mov DL , 'n'
+int 21h
+mov DL , 'c'
+int 21h
+mov DL , 'e'
+int 21h
+mov DL , 's'
+int 21h
+mov DL , 'c'
+int 21h
+mov DL , 'o'
+int 21h
+mov DL , ' ' 
+int 21h
+mov DL , 'F'
+int 21h
+mov DL , 'o'
+int 21h
+mov DL , 'n'
+int 21h
+mov DL , 't'
+int 21h
+mov DL , 'a'
+int 21h
+mov DL , 'n'
+int 21h
+mov DL , 'e'
+int 21h
+mov DL , 's'
+int 21h
+mov DL , 'i'
+int 21h
+mov DL , ' '
+int 21h
+mov DL , '0'
+int 21h
+mov DL , '7'
+int 21h
+mov DL , '/'
+int 21h
+mov DL , '0'
+int 21h
+mov DL , '4'
+int 21h     
+MOV DL, '/'
+int 21h
+MOV DL, '2'
+int 21h 
+MOV DL, '0'
+int 21h
+MOV DL, '0'
+int 21h
+MOV DL, '8'
+int 21h
+
+
+ret
